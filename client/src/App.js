1acc380482b79854209266d4be90ff8392f99ab6
listok222 <listok222@example.com>
2025-09-25 01:03:02 +0300

    Final project structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Messenger App</h1>
+      <p>Frontend is working!</p>
+    </div>
+  );
+}
+
+export default App;
